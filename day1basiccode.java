952af742c5fdd1229ab604163b736a2e20803329
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2025-05-18 18:33:24 +0530

    Add files via upload

---
+++ day1basiccode.java
@@ -0,0 +1,5 @@
+class day1basiccode{
+    public static void main(String s[]) {
+        System.out.println("java begginning");
+    }
+}
---
+++ day1comments.java
@@ -0,0 +1,9 @@
+//creating a class
+class day1comments{
+    public static void main(String s[]) {
+/*printing the data 
+data which you like
+*/
+        System.out.println("java begginning");
+    }
+}
