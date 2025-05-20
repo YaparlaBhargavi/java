5c4a17cc9cc8f04f180e957d12505cc0d2a76c71
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2025-05-20 18:28:56 +0530

    Add files via upload

---
+++ day3operaters.java
@@ -0,0 +1,72 @@
+class day3operaters {
+    public static void main(String args[]){
+
+        //arthimatic operations
+       /*   int a=5 ,b=2;
+        System.out.println("a+b: "+(a+b));
+        System.out.println("a-b: "+(a-b));
+        System.out.println("a*b: "+(a*b));
+        System.out.println("a/b: "+(a/b));
+        System.out.println("a%b: "+(a%b));
+        
+        
+        //assignment operators
+         
+        int a=1;
+        System.out.println("a+=3 :"+ (a+=3));
+        System.out.println("a-=2 :"+ (a-=2));
+        System.out.println("a*=4 :"+ (a*=4));
+        System.out.println("a/=5 :"+ (a/=5));
+
+    
+    
+    //relational operators
+     
+    int a=8,b=5;
+    System.out.println("a==b :"+ (a==b));
+    System.out.println("a>b :"+ (a>b));
+    System.out.println("a>=b :"+ (a>=b));
+    System.out.println("a<b :"+ (a<b));
+    System.out.println("a<=b :"+ (a<=b));
+    System.out.println("a!=b :"+ (a!=b));
+
+    
+
+    //logical operators
+    
+    boolean a=true ,b=false ;
+    System.out.println("a&&b :"+ (a && b));
+    System.out.println("a||b :"+ (a || b));
+    System.out.println("!a :"+ (!a));
+
+
+
+    //unary operators
+    
+    int a=10,b=10;
+    System.out.println("postincrement:"+(a++));
+    System.out.println("preincrement:"+(++a));
+    System.out.println("postdecrement:"+(b--));
+    System.out.println("predecrement:"+(--b));
+     
+    
+
+     //bitwise operators
+      
+     int a=5,b=3;
+     System.out.println("a & b :"+(a &b));
+     System.out.println("a | b :"+(a |b));
+     System.out.println("~a :"+(~a));
+     System.out.println("a^ b :"+(a ^ b));
+     System.out.println("a<< 1:"+(a << 1));
+     System.out.println("a >> 2:"+( a >> 2));
+     System.out.println(" a >>> 1:"+(a >>> 1));
+
+     //ternary operators
+    int a=2 ,b =4,max;
+    max=(a>b)?a:b;
+    System.out.println(max);
+    */
+    }
+    
+}
