0c5371d1c9c29a364c177e88d97649c5dfdbbe0e
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2025-05-25 13:53:13 +0530

    Add files via upload

---
+++ day5breakstatement.java
@@ -0,0 +1,11 @@
+ class day5breakstatement {
+    public static void main(String[] args) {
+        for(int i=0;i<50;i++){
+            System.out.println("amma is a previous one"+(i));
+        if(i==30){
+            break;
+        }
+    }
+    }
+    
+}
---
+++ day5continuestatement.java
@@ -0,0 +1,12 @@
+class day5continuestatement {
+    public static void main(String[] args) {
+        for(int i=0;i<20;i++)
+        {
+        if(i==4){
+            continue;
+        }
+        System.out.println(i);
+      
+}
+}
+} 
