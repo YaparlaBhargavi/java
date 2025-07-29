89a9d6fc03ebe749369ca4c4a7dc32c96cf309c6
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2025-07-29 20:37:19 +0530

    Update Day6array.java

--- Day6array.java
+++ Day6array.java
@@ -1,15 +1,7 @@
 public class Day6array{
     public static void main(String[] args) {
-        //int a[]={5,8,9,6};//array declaration
-        //System.out.println(a[3]);//array accessing a element
-
-      //changing of array elements
-
-        /* int a[]={88,98,65,76};
-        System.out.println("before:"+(a[0]));
-        a[0]=75;
-        System.out.println("after:"+(a[0]));
-        */
+        out.println("after:"+(a[0]));
+    
 
         //length of array
         int a[]={88,76,85,84,76,34,87,65,43};
